f3a36f4ef95025b82fb7800dae1a6ffec006657c
niryn <nautiyalaman93@gmail.com>
2026-04-15 00:33:52 +0530

    Fix: Set absolute callbackURL to resolve Bad Request error

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/auth/google/callback',
+      callbackURL: 'https://bhartpath-api.onrender.com/api/auth/google/callback',
       proxy: true,
       state: false,
     },
